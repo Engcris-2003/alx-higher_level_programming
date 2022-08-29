d55d832963e3e421e2d8a5f7684bc0339944aebe
Engcris-2003 <atiochristabel639@gmail.com>
2022-08-29 03:12:24 -0700

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    number_of_arg = len(argv) - 1
+    if number_of_arg < 1:
+        print("{} arguments.".format(number_of_arg))
+    elif number_of_arg == 1:
+        print("{} argument:".format(number_of_arg))
+        print("{}: ".format(number_of_arg) + argv[1])
+    elif number_of_arg > 1:
+        print("{} arguments:".format(number_of_arg))
+        for i in range (1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
