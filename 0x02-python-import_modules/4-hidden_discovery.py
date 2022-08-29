0a99b7cdd49efcba00a6e5119571cf494fb3f969
Engcris-2003 <atiochristabel639@gmail.com>
2022-08-29 05:33:10 -0700

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    dir_list = dir(hidden_4)
-    for 1st in dir_list:
-        if 1st[:2] != "__"
-            print(1st)
+    dir(hidden_4)
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print(i)
